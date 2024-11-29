88148594a357731107e9257c173e4dc164ca217b
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-11-29 14:15:26 +0100

    456

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,7 +2,6 @@ import { readContacts } from '../utils/readContacts.js';
 
 export const getAllContacts = async () => {
   const arr = await readContacts();
-  console.log(arr);
 };
 
 getAllContacts();
